54b51d3bad5ce2e5d5c4d154f9563f482c2a2a35
Pamindu-Rashmitha <pamidurashmitha@gmail.com>
2026-04-14 13:14:07 +0530

    updated Product routes to follow best practices

--- granite-system-backend/routes/productRoutes.js
+++ granite-system-backend/routes/productRoutes.js
@@ -5,13 +5,14 @@ const {
     createProduct,
     getProducts,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    reviewOrder
 } = require('../controllers/productController');
 const {protect} = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware');
 
 router.route('/').get(getProducts).post(protect, upload.array('images', 5), productValidation, validate, createProduct);
-router.route('/review-order').post(protect, require('../controllers/productController').reviewOrder);
+router.route('/review-order').post(protect,reviewOrder);
 router.route('/:id').put(protect, upload.array('images', 5), productValidation, validate, updateProduct).delete(protect, deleteProduct);
 
 module.exports = router;
